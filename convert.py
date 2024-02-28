7b163f3a569f8a0bb8dc8014d2a37680d85987c1
Jooyoung Lee <jylee8875@gmail.com>
2024-02-28 15:38:45 +0900

    Update convert.py
    update

--- convert.py
+++ convert.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-
 """
-This is a simple (and probably broken) Python script that converts BibTeX
-entries to CSV.
+이 코드는 아래 코드를 포크하여 수정한 것임.
+Reference code on GitHub
+https://github.com/fluffels/bibtex-csv/blob/master/convert.py
+////////////////////////////////
 
-Input is via standard input.
-Output is via standard output.
+사용법
+자세한 내용은 README.md 참조
+python3 conv-jylee.py < test.bib > output.csv
 """
 
 from os import getcwd
@@ -18,6 +20,9 @@ from string import capwords
 
 entries = []
 entry = {}
+#regist: example of internal members
+#알고 있는 멤버 확인
+regist = ["J. Y. Lee"]
 
 for line in stdin:
     if (match('^@', line.strip())):
@@ -31,43 +36,75 @@ for line in stdin:
         key, value = [v.strip(" {},\n") for v in line.split("=", maxsplit=1)]
         entry[key] = value
 
+# csv 파일 상단의 제목 (optional)
+# 아래 실제 정보과 일치하는지 확인 필요
+# 데이터 구분을 위해 "|" 사용
+# "공백", "," 등의 문자도 사용 가능
+print("Title|Journal|ISSN|DOI|Vol|Issue|Year|Month|page|Number of Author|Authors")
+
 entries.append(entry)
 
 for entry in entries:
-    author = "Anonymous"
+    author = "NA"
     if "author" in entry:
         author = entry["author"]
     elif "authors" in entry:
         author = entry["authors"]
-    elif "editor" in entry:
-        author = entry["editor"]
-    
-    title = "No title"
+    spAu = author.split('and')
+    #총 저자 수
+    Nauthor = len(spAu)
+    new_list = [i.strip() for i in spAu]
+    #저자 (resist에 저장된 저자만 출력)
+    f_au = set(new_list) & set(regist)
+    author = ','.join(f_au)
+
+    #논문 제목
+    title = "NA"
     if "title" in entry:
         title = entry["title"]
 
-    publish = "No publishing information"
+    #게제 저널
+    journal = "NA"
     if "journal" in entry:
-        publish = entry["journal"]
-    if "journaltitle" in entry:
-        publish = entry["journaltitle"]
-    elif "booktitle" in entry:
-        publish = entry["booktitle"]
-    elif "howpublished" in entry:
-        publish = entry["howpublished"]
-    elif "type" in entry:
-        publish = entry["type"]
-    elif "url" in entry:
-        publish = "Website: {}".format(entry["url"])
-    elif "crossref" in entry:
-        publish = entry["crossref"].replace("_", " ")
-        publish = capwords(publish)
-    elif "publisher" in entry:
-        publish = entry["publisher"]
-    
-    year = "Unknown year"
+        journal = entry["journal"]
+   
+    #게제 연도
+    year = "NA"
     if "year" in entry:
         year = entry["year"]
 
-    print("{}\t{}\t{}\t{}".format(author, title, year, publish))
+    #게제 월
+    month= "NA"
+    if "month" in entry:
+        month = entry["month"]
 
+    #페이지
+    pages = "NA"
+    if "pages" in entry:
+        pages = entry["pages"]
+
+    #권
+    volume = "NA"
+    if "volume" in entry:
+        volume = entry["volume"]
+
+    #이슈
+    issue = "NA"
+    if "issue" in entry:
+        issue = entry["issue"]
+
+    #게제 저널 ISSN
+    issn = "NA"
+    if "issn" in entry:
+        issn = entry["issn"]
+
+    #논문 DOI
+    doi = "NA"
+    if "doi" in entry:
+        doi = entry["doi"]
+
+    print("{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}".format(title, journal, issn, doi, volume, issue, year, month, pages, Nauthor, author))
+
+# Python 기본 문법 확인
+# 현재 코드는 [논문 제목, 저널 이름, ISSN, DOI, Vol, Issue, 게제 연도, 게제 월, 페이지, 총 저자 수, 저자] 데이터 출력
+# 미리 등록된 저자와 논문 저자와 비교하여 일치하는 경우만 저자 출력
